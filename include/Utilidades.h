4d0eea12bcb282c8627840e68ed259c3179fd6a5
EstebanValero404 <esteban2008games@gmail.com>
2026-05-13 13:39:53 -0500

    correccion de utilidades.h

--- include/Utilidades.h
+++ include/Utilidades.h
@@ -2,9 +2,12 @@
 #include <cstdlib>
 
 #ifdef _WIN32
+    #include <conio.h>
     #include <windows.h>
     #define LIMPIAR system("cls")
 #else
+    #include <termios.h>
+    #include <unistd.h>
     #define LIMPIAR system("clear")
 #endif
 
