35a51cd8cb806b2695d3287eeff28a17f126f65e
EstebanValero404 <esteban2008games@gmail.com>
2026-05-14 16:11:57 -0500

    correcion de juego.h

--- include/juego.h
+++ include/juego.h
@@ -1,12 +1,11 @@
 #pragma once
 #include "tipos.h"
-
 bool moverJugador(Jugador* jugador, char tecla);
-
 void moverEnemigos(Jugador* jugador);
-
 bool hayColisionEnemigo(Jugador* jugador);
-
 void recogerObjeto(Jugador* jugador);
-
 bool verificarVictoria(Jugador* jugador);
+void pantallaInicio();
+void mostrarEstado(Jugador* jugador, bool danio);
+void pantallaFin(bool victoria, int vidas);
+void correrJuego();
