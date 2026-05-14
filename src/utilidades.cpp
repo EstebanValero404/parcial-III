3d9f09362b2b2be3f6031d0fbbe4ff6f22a83ef7
EstebanValero404 <esteban2008games@gmail.com>
2026-05-14 15:38:17 -0500

    añadir utilidades del juego

--- src/utilidades.cpp
+++ src/utilidades.cpp
@@ -1,1 +1,18 @@
-
+#include "../include/utilidades.h"
+#ifdef _WIN32
+char leerTecla() {
+    return (char)_getch();
+}
+#else
+char leerTecla() {
+    struct termios oldt, newt;
+    char ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    read(STDIN_FILENO, &ch, 1);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+#endif
