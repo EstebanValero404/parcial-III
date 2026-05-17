8a1f57507b95c9a2fa075cb871f9131335b159bf
sebas2007-ops <sebasju2020@gmail.com>
2026-05-17 10:05:14 -0500

    Update Utilidades.h to include SDL and new functions
    mejoramos el juego a sdl, trabajo probado en visual studio

--- include/Utilidades.h
+++ include/Utilidades.h
@@ -1,14 +1,14 @@
 #pragma once
-#include <cstdlib>
+#include <SDL.h>
+#define TILE_SIZE 32
+#define ANCHO_VENTANA (22 * TILE_SIZE)
+#define ALTO_VENTANA  (12 * TILE_SIZE)
 
-#ifdef _WIN32
-    #include <conio.h>
-    #include <windows.h>
-    #define LIMPIAR system("cls")
-#else
-    #include <termios.h>
-    #include <unistd.h>
-    #define LIMPIAR system("clear")
-#endif
+extern SDL_Window*   gVentana;
+extern SDL_Renderer* gRenderer;
 
+bool iniciarSDL();
+void cerrarSDL();
 char leerTecla();
+
+void dibujarRect(int x, int y, int w, int h, Uint8 r, Uint8 g, Uint8 b);
