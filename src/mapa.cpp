c61703cebb30899471a64147a0b24d9f3b542ff6
sebas2007-ops <sebasju2020@gmail.com>
2026-05-17 10:07:47 -0500

    Refactor room drawing and add SDL rendering

--- src/mapa.cpp
+++ src/mapa.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "../include/tipos.h"
 #include "../include/mapa.h"
+#include "../include/utilidades.h"
+
 Habitacion habitaciones[NUM_HABITACIONES];
 Enemigo    enemigos[MAX_ENEMIGOS];
 int        totalEnemigos = 0;
+
 static void construirHabitacion(int idx) {
     Habitacion* hab = &habitaciones[idx];
     for (int i = 0; i < FILAS; i++)
@@ -20,8 +23,9 @@ static void construirHabitacion(int idx) {
     for (int d = 0; d < 4; d++)
         hab->conexiones[d] = -1;
     hab->tieneObjeto = false;
-    hab->esSalida = false;
+    hab->esSalida    = false;
 }
+
 static void agregarPuerta(int idx, int dir, int destino) {
     Habitacion* hab = &habitaciones[idx];
     hab->conexiones[dir] = destino;
@@ -39,6 +43,7 @@ static void agregarPuerta(int idx, int dir, int destino) {
         hab->mapa[PUERTA_FILA2][COLS-1] = ' ';
     }
 }
+
 void inicializarHabitaciones() {
     for (int i = 0; i < NUM_HABITACIONES; i++)
         construirHabitacion(i);
@@ -67,6 +72,7 @@ void inicializarHabitaciones() {
     habitaciones[5].mapa[4][11] = '#';
     habitaciones[5].mapa[4][12] = '#';
 }
+
 void inicializarEnemigos() {
     totalEnemigos = 0;
     enemigos[totalEnemigos++] = {{2, 18}, FANTASMA,  true, 0, 1};
@@ -77,27 +83,76 @@ void inicializarEnemigos() {
 
 void dibujarHabitacion(int habitacionActual, Jugador* jugador) {
     Habitacion* hab = &habitaciones[habitacionActual];
-    char display[FILAS][COLS];
-    for (int i = 0; i < FILAS; i++)
-        for (int j = 0; j < COLS; j++)
-            display[i][j] = hab->mapa[i][j];
-    if (hab->tieneObjeto)
-        display[hab->posObjeto.fila][hab->posObjeto.col] = 'K';
-    if (hab->esSalida)
-        display[hab->posSalida.fila][hab->posSalida.col] = 'E';
-    for (int i = 0; i < totalEnemigos; i++) {
-        Enemigo* e = &enemigos[i];
-        if (e->activo && e->habitacion == habitacionActual) {
-            char simbolo = (e->tipo == FANTASMA) ? 'G' : 'S';
-            display[e->pos.fila][e->pos.col] = simbolo;
+
+    SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
+    SDL_RenderClear(gRenderer);
+
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            int x = j * TILE_SIZE;
+            int y = i * TILE_SIZE;
+            char celda = hab->mapa[i][j];
+
+            if (celda == '#') {
+            
+                dibujarRect(x, y, TILE_SIZE, TILE_SIZE, 80, 80, 80);
+                
+                dibujarRect(x+2, y+2, TILE_SIZE-4, TILE_SIZE-4, 60, 60, 60);
+            } else {
+                
+                dibujarRect(x, y, TILE_SIZE, TILE_SIZE, 40, 25, 10);
+            }
         }
     }
-    display[jugador->pos.fila][jugador->pos.col] = '@';
-    std::cout << " CALABOZO OSCURO | Habitacion "
-              << (habitacionActual + 1) << " de " << NUM_HABITACIONES << "\n";
-    for (int i = 0; i < FILAS; i++) {
-        for (int j = 0; j < COLS; j++)
-            std::cout << display[i][j];
-        std::cout << "\n";
+
+    
+    if (hab->tieneObjeto) {
+        int x = hab->posObjeto.col * TILE_SIZE;
+        int y = hab->posObjeto.fila * TILE_SIZE;
+        
+        dibujarRect(x+8,  y+12, 16, 8,  255, 220, 0);
+        dibujarRect(x+18, y+8,  6,  16, 255, 220, 0);
     }
+
+    
+    if (hab->esSalida) {
+        int x = hab->posSalida.col * TILE_SIZE;
+        int y = hab->posSalida.fila * TILE_SIZE;
+        
+        dibujarRect(x+4, y+4, TILE_SIZE-8, TILE_SIZE-8, 0, 200, 0);
+        dibujarRect(x+10, y+10, TILE_SIZE-20, TILE_SIZE-20, 0, 255, 0);
+    }
+
+    
+    for (int i = 0; i < totalEnemigos; i++) {
+        Enemigo* e = &enemigos[i];
+        if (!e->activo || e->habitacion != habitacionActual) continue;
+        int x = e->pos.col  * TILE_SIZE;
+        int y = e->pos.fila * TILE_SIZE;
+        if (e->tipo == FANTASMA) {
+            
+            dibujarRect(x+4,  y+8,  24, 16, 100, 150, 255);
+            dibujarRect(x+8,  y+4,  16, 8,  100, 150, 255);
+            dibujarRect(x+10, y+10, 4,  4,  255, 255, 255);
+            dibujarRect(x+18, y+10, 4,  4,  255, 255, 255);
+        } else {
+            
+            dibujarRect(x+10, y+2,  12, 12, 220, 210, 190);
+            dibujarRect(x+12, y+14, 8,  14, 220, 210, 190);
+            dibujarRect(x+6,  y+16, 6,  10, 220, 210, 190);
+            dibujarRect(x+20, y+16, 6,  10, 220, 210, 190);
+        }
+    }
+
+    
+    {
+        int x = jugador->pos.col  * TILE_SIZE;
+        int y = jugador->pos.fila * TILE_SIZE;
+        dibujarRect(x+8,  y+4,  16, 16, 255, 50, 50);
+        dibujarRect(x+10, y+20, 12, 8,  255, 50, 50);
+        dibujarRect(x+6,  y+22, 6,  8,  255, 50, 50);
+        dibujarRect(x+20, y+22, 6,  8,  255, 50, 50);
+    }
+
+    SDL_RenderPresent(gRenderer);
 }
