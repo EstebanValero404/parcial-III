ba6e015e5882d23aa2fadd009150fea9f2566382
sebas2007-ops <sebasju2020@gmail.com>
2026-05-19 20:19:56 -0500

    Delete include directory

--- include/mapa.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "tipos.h"
-extern Habitacion habitaciones[NUM_HABITACIONES];
-extern Enemigo enemigos[MAX_ENEMIGOS];
-extern int totalEnemigos;
-void inicializarHabitaciones();
-void inicializarEnemigos();
-void dibujarHabitacion(int habitacionActual, Jugador* jugador);
